2dcf22e1f45b80b6b9cdd870b98c8ee16a4c0986
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2023-12-28 22:18:55 +0530

    changed tests

---
+++ tests/numbers.c
@@ -0,0 +1,2 @@
+3.14159 12 0xff 016
+// 0b22 0x3.1 019 12.12.12 // <- throws errors when uncommented
--- tests/whitespaces_comments.c
+++ tests/whitespaces_comments.c
@@ -1,5 +1,5 @@
 // much whitespace
 /* much commento */
-// end end the end hahaha int main() 
-   /*  { this is a comment not code haha } */
-/* bru */
+// end end the end hahaha int main()
+/*  { this is a comment not code haha }
+ */ // <- commenting this line throws an error
